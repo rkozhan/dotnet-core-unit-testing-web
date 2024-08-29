76d9fb64fe48f3405dc2a79080e9f49da0bf3d7a
rkozhan~ <yalta.vrk@gmail.com>
2024-08-29 12:44:23 +0200

    testing HttpGet API endpoint

--- LibraryAPI.Test/BooksControllerTest.cs
+++ LibraryAPI.Test/BooksControllerTest.cs
@@ -1,6 +1,9 @@
 using Library.API.Controllers;
+using Library.API.Data.Models;
 using Library.API.Data.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LibraryAPI.Test
@@ -14,6 +17,23 @@ namespace LibraryAPI.Test
         {
             _service = new BookService();
             _controller = new BooksController(_service);
+        }
+
+        [Fact]
+        public void GetAllTest ()
+        {
+            //arrange
+            //act
+            var result = _controller.Get();
+            //assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var list = result.Result as OkObjectResult;
+            Assert.IsType<List<Book>>(list.Value);
+
+            var lisrBooks = list.Value as List<Book>;
+
+            Assert.Equal(5, lisrBooks.Count);
         }
 
     }
